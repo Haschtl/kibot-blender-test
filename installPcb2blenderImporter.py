d4acbf5fd1eacf0c165f5145342dad3f9a0598c2
GardenCoder <haschtl@protonmail.com>
2023-09-23 12:50:08 +0000

    fix

--- installPcb2blenderImporter.py
+++
@@ -1,5 +0,0 @@
-import bpy
-
-bpy.ops.preferences.addon_install(overwrite=True, filepath='./pcb2blender_importer_2-6.zip')
-bpy.ops.preferences.addon_enable(module='pcb2blender_importer')
-bpy.ops.wm.save_userpref()
